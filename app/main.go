d396f1235495ea47b7f7a6a2843ea5ee58bedfeb
Huamin Chen <hchen@redhat.com>
2018-02-12 16:17:34 +0000

    add kubeconfig
    Signed-off-by: Huamin Chen <hchen@redhat.com>

--- app/main.go
+++ app/main.go
@@ -13,6 +13,7 @@ import (
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 const (
@@ -22,14 +23,28 @@ const (
 	defaultConfigMapNamespace    = "default"
 )
 
+var (
+        kubeConfig      string                                                                                                                                                              
+        kubeMaster      string  
+)
 func main() {
 	flag.StringVar(&controller.IntializerAnnotation, "annotation", defaultInitializerAnnotation, "The annotation to trigger initialization")
 	flag.StringVar(&controller.IntializerConfigmapName, "configmap", defaultConfigmapName, "storage initializer configuration configmap")
 	flag.StringVar(&controller.InitializerName, "initializer-name", defaultInitializerName, "The initializer name")
 	flag.StringVar(&controller.IntializerNamespace, "namespace", defaultConfigMapNamespace, "The configuration namespace")
+	flag.StringVar(&kubeConfig, "kubeconfig", "", "Absolute path to the kubeconfig")
+	flag.StringVar(&kubeMaster, "kubemaster", "", "Kubernetes Controller Master URL")
 	flag.Parse()
+	flag.Set("logtostderr", "true")
 
-	clusterConfig, err := rest.InClusterConfig()
+	var clusterConfig *rest.Config
+	var err error
+	if len(kubeMaster) > 0 || len(kubeConfig) > 0 {
+		clusterConfig, err = clientcmd.BuildConfigFromFlags(kubeMaster, kubeConfig)
+	} else {
+		clusterConfig, err = rest.InClusterConfig()
+	}
+
 	if err != nil {
 		glog.Fatal(err.Error())
 	}
@@ -41,7 +56,7 @@ func main() {
 
 	_, err = clientset.CoreV1().ConfigMaps(controller.IntializerNamespace).Get(controller.IntializerConfigmapName, metaV1.GetOptions{})
 	if err != nil {
-		glog.Fatal(err)
+		glog.Warning(err)
 	}
 
 	ctrl, err := controller.NewPVInitializer(clientset)
